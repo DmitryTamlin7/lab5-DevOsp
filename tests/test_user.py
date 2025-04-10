8bf4ae398cf42575207427210caabad179ec22c6
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2025-04-10 19:48:00 +0300

    fix 4

--- tests/test_user.py
+++ tests/test_user.py
@@ -17,20 +17,17 @@ users = [
     }
 ]
 
-
 def test_get_existed_user():
     '''Получение существующего пользователя'''
     response = client.get("/api/v1/user", params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
-
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
     response = client.get("/api/v1/user", params={'email': 'non.existent@mail.com'})
     assert response.status_code == 404
     assert response.json() == {"detail": "User not found"}
-
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
@@ -42,12 +39,13 @@ def test_create_user_with_valid_email():
     assert response.status_code == 201
     data = response.json()
 
+    print(data)  # Отладочный вывод
+
     # Проверка, что ответ имеет правильную структуру
     assert isinstance(data, dict)  # Проверяем, что это словарь
     assert 'id' in data
     assert data['name'] == new_user['name']
     assert data['email'] == new_user['email']
-
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
@@ -58,8 +56,7 @@ def test_create_user_with_invalid_email():
     }
     response = client.post("/api/v1/user", json=new_user)
     assert response.status_code == 409  # Ожидаем код 409 Conflict
-    assert response.json() == {"detail": "User already exists"}
-
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
@@ -72,6 +69,7 @@ def test_delete_user():
 
     # Проверка, что ответ в правильном формате
     create_data = create_response.json()
+    print(create_data)  # Отладочный вывод
     assert isinstance(create_data, dict)  # Проверяем, что это словарь
     user_id = create_data['id']
 
